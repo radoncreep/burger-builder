1de405f0df1830de6d7431c96d44bcaf0e51b6a0
Victor Onofiok <radoncreep@gmail.com>
2020-07-17 12:59:39 +0100

    order

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import './Input.css';
+
+const input = (props) => {
+    let inputElement = null;
+
+    switch (props.elementType) { // checking what kind of element/tag it is, maybe input tag or textarea html tag
+        case ('input'): 
+            inputElement = <input 
+                className="InputElement" 
+                {...props.elementConfig} 
+                value={props.value}
+                onChange={props.changed}
+            />; 
+        break;
+
+        case ('textarea'):
+            inputElement = <textarea 
+                className="InputElement" 
+                {...props.elementConfig} 
+                value={props.value}
+                onChange={props.changed}
+            />;
+        break;
+
+        case ( 'select'  ):
+            inputElement = (
+                <select
+                    className="InputElement" 
+                    value={props.value}
+                    onChange={props.changed}>
+                    {/* value is a compulsory attr for elements */}
+                    {props.elementConfig.options.map(option => ( // either use return or () to avoid the expected function call or return value
+                        <option key={option.value} value={option.value}>{option.displayValue}</option>
+                    ))}
+                </select>
+            )
+        break;
+
+        default:  // ALT + Clicks let's you writing same code on multiple lines
+            inputElement = <input 
+                className="InputElement" 
+                {...props.elementConfig} 
+                value={props.value}
+                onChange={props.changed} 
+            />;
+    }
+
+    return (
+        <div className="Input">
+            <label className="Label">{props.label}</label>
+            { inputElement }
+        </div>
+    );
+};
+
+export default input;
+
+// NOTE 
+// inputElement = <textarea className="InputElement" {...props}/>
+// {...props}  spreads the properties being passed into the props object
+// from the input JSX component used in ContactData file/module/component
+// the properties of the Jsx such as input name type placeholder 
+// is being passed here into props and spread as properties of the prop obj
+// having something like 
+// const props = {
+//     type: 'text/password/email',
+//     name: 'name/password/email',
+//     placeholder: 'Your email/Your name'
+// }
+// so the JSX attributes are being spread as properties into the props obj
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,35 +4,77 @@ import axios from '../../../axios-orders';
 import './ContactData.css'
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
-        },
-        loading: false
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your name'
+                },
+            value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your email'
+                },
+                value: ''
+            },
+            deliveryMethod:  {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fatest', displayValue: 'fastest'},
+                        {value: 'cheapest', displayValue: 'cheapest'}
+                    ]
+                },
+                value: ''
+            },
+        }
     }
 
     orderHandler = (event) => {
         event.preventDefault(); // this method prevents the default, which would be to send a req and reload the page
         this.setState({ loading: true }); // to show spinner if we want to 
 
+        let formData = {};
+
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         // alert('You continue to the next step!');
         const orders = {
             ingredients: this.props.ingredients,
             price: this.props.price, // this should be recalculated in the server in a real application
-            customer: {
-                name: 'Victor Onofiok',
-                address: {
-                    street: 'Wall Street',
-                    zipCode: '019234',
-                    country: 'USA'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fatest'
+            orderData: formData
         };
 
         // the syntax for sending a req to firebase is the name of the node with .json as its extension
@@ -50,13 +92,44 @@ class ContactData extends Component {
         );
     }
 
+    inputChangedHandler = (event, inputIdentifier) => { // Two way binding
+        // console.log(event.target.value);
+        const updatedOrderForm = { // cloning the order form object
+            ...this.state.orderForm
+        };
+        const updateFormElement = { // referencing the properties of the order form clone
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updateFormElement.value = event.target.value; // not [value] but .value first one is assigning a value to the value of property while the second is assiging a value to the property or key
+        updatedOrderForm[inputIdentifier] = updateFormElement // sp here youre assigning the cloned properties to the clone order form itself
+        this.setState({ orderForm: updatedOrderForm});
+    };
+
     render() {
+        let formElementsArray = [];
+
+        for (let key in this.state.orderForm) { // each of the properties of order form are going to be object elements
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key], // this is going to be an obje ct like name: {} , street: {} as in this.state.orderForm
+            });
+        };
+
         let form = (
             <form>
-                <input className="Input" type="text" name="name" placeholder="Your name"></input>
-                <input className="Input" type="email" name="email" placeholder="Your email"></input>
-                <input className="Input" type="text" name="street" placeholder="Your street address"></input>
-                <input className="Input" type="text" name="postal" placeholder="Your postal code"></input>
+                {/* <Input elementType="..." elementConfig="..." value="..." /> */}
+                {formElementsArray.map(formElement => { // it's either you use the return keyword or you return the component as o line with => () instead of => {} where you need the return kwyword
+                    return (
+                        <Input 
+                            key={formElement.id}
+                            elementType={formElement.config.elementType}
+                            elementConfig={formElement.config.elementConfig}
+                            value={formElement.config.value}
+                            changed={(event) => this.inputChangedHandler(event, formElement.id)}
+                        />
+                    )
+                })}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
